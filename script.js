a617e5de6304f826bb54628b7e0524b244060a30
brian <pbpatterson92@gmail.com>
2022-05-31 18:42:27 -0700

    make letsPlay function

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ let myArray= [
     "scissors",
 ];
 
-let x="rock";
-let y="paper";
-let z="scissors";
-
 function computerPlay() {
    let item = myArray[Math.floor(Math.random()*myArray.length)]
-   return item 
+   return item
 }
+
+function letsPlay(playerSelection, computerSelection) {
+    playerSelection = "rock";
+    computerSelection = computerPlay();
+
+    switch (playerSelection+computerSelection) {
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            result = "you lose"; 
+            break;
+        case "paperrock":
+        case "scissorspaper":
+        case "rockscissors":
+            result = "you win";
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            result = "it's a tie"
+            break;       
+    }
+    return result; 
+
+};
