d60f89f53735f4cad004ad206b8b8639f542633d
brian <pbpatterson92@gmail.com>
2022-06-19 11:57:17 -0700

    first to 5

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ const score = document.querySelector('#score');
 
 const pScore = document.createElement('div');
 pScore.classList.add('pScore');
-pScore.textContent = "Player Score: 0";
+pScore.textContent = "Player Wins: 0";
 score.appendChild(pScore);
 
 const cScore = document.createElement('div');
 cScore.classList.add('cScore');
-cScore.textContent = "Computer Score: 0";
+cScore.textContent = "Computer Wins: 0";
 score.appendChild(cScore);
 
 const tieGame = document.createElement('div');
@@ -39,6 +39,18 @@ tieGame.classList.add('tieGame');
 tieGame.textContent = "Tie Game: 0";
 score.appendChild(tieGame); 
 
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    pScore.textContent = "Player Wins: 0";
+    cScore.textContent = "Computer Wins: 0";
+    tieGame.textContent = "Tie Games: 0";
+    pWins = 1;
+    cWins = 1;
+    tGame = 1;
+    document.getElementById("rBtn").disabled=false;
+    document.getElementById("pBtn").disabled=false;
+    document.getElementById("sBtn").disabled=false; 
+})
 let pWins = 1;
 let cWins = 1;
 let tGame = 1; 
@@ -72,16 +84,29 @@ function tally () {
     if (log == "you win"){
         pScore.textContent = `Player Wins: ${pWins++}`;
     } else if (log == "you lose"){
-        cScore.textContent = `Computer Sore: ${cWins++}`;
+        cScore.textContent = `Computer Wins: ${cWins++}`;
     } else if (log == "it's a tie"){
-        tieGame.textContent = `Tie Game: ${tGame++}`;
+        tieGame.textContent = `Tie Games: ${tGame++}`;
     }
 }
 
+let disable = document.querySelectorAll(".btn");
+
+function disableBtn () {
+    disable.disabled = true;
+};
+
 function declaration () {
-    if (pWins > 4){
+    if (pWins > 5){
         pScore.textContent = "PLAYER WINS!!!";
-    } else if (cWins > 4){
+        document.getElementById("rBtn").disabled=true;  
+        document.getElementById("pBtn").disabled=true;
+        document.getElementById("sBtn").disabled=true;
+    } else if (cWins > 5){
         cScore.textContent = "COMPUTER WINS!!!";
+        document.getElementById("rBtn").disabled=true;
+        document.getElementById("pBtn").disabled=true;
+        document.getElementById("sBtn").disabled=true;
     }
 }
+
